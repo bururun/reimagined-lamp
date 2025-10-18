cedb68df7233ddb0a537ac8daecd452dab2804da
bururun <bururun@users.noreply.github.com>
2025-10-18 17:35:15 +0300

    chore: add feature flag

--- src/index.js
+++ src/index.js
@@ -171,3 +171,28 @@ class LampServer {
 
 // Update 41
 module.exports = LampServer;
+
+
+// Main entry point for LampServer
+
+class LampServer {
+    constructor() {
+        this.initialized = true;
+        this.version = '1.0.66';
+    }
+    
+    start() {
+        return 'Started';
+    }
+    
+    stop() {
+        return 'Stopped';
+    }
+    
+    getStatus() {
+        return { status: 'running', version: this.version };
+    }
+}
+
+// Update 66
+module.exports = LampServer;
