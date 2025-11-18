0420f8fd41989f6c4872406b9ecc1d527815f7ee
bururun <bururun@users.noreply.github.com>
2025-11-18 11:17:06 +0200

    improve: fix security vulnerability

--- src/utils.js
+++ src/utils.js
@@ -20,3 +20,27 @@ function newFunction6() {
 }
 
 module.exports = { formatData, validateInput };
+
+
+// Utility functions for LampServer
+
+function formatData(data) {
+    if (typeof data === 'string') {
+        return data.trim().toUpperCase();
+    }
+    return data;
+}
+
+function validateInput(value) {
+    if (value === null || value === undefined) {
+        throw new Error('Value cannot be null');
+    }
+    return true;
+}
+
+// Update 9
+function newFunction9() {
+    return 9;
+}
+
+module.exports = { formatData, validateInput };
