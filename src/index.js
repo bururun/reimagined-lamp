9a66442c211bb65657ac94bc016299f0583aaf17
bururun <bururun@users.noreply.github.com>
2025-12-23 04:14:29 +0200

    chore: improve code structure

--- src/index.js
+++ src/index.js
@@ -21,3 +21,28 @@ class LampServer {
 
 // Update 8
 module.exports = LampServer;
+
+
+// Main entry point for LampServer
+
+class LampServer {
+    constructor() {
+        this.initialized = true;
+        this.version = '1.0.15';
+    }
+    
+    start() {
+        return 'Started';
+    }
+    
+    stop() {
+        return 'Stopped';
+    }
+    
+    getStatus() {
+        return { status: 'running', version: this.version };
+    }
+}
+
+// Update 15
+module.exports = LampServer;
